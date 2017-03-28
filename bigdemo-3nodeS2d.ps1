fb4313424bc41f5541733f6b58424fd73dd29f51
dkawula <dkawula@triconelite.com>
2017-03-28 10:29:42 -0600

    Update bigdemo-3nodeS2d.ps1

--- bigdemo-3nodeS2d.ps1
+++ bigdemo-3nodeS2d.ps1
@@ -6,7 +6,7 @@
 
     Disclaimer:
     This script is provided "AS IS" with no warranties, confers no rights and 
-    is not supported by the authors or DeploymentArtist.
+    is not supported by the authors
 
     Author - Dave Kawula
     Twitter: @DaveKawula
